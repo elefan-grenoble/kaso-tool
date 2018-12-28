1081131d89fae0e8be4d91d61839b9445f3475b6
Bastien Deshayes <deshayeb@gmail.com>
2018-12-28 01:14:40 +0100

    Ajout du script d'initialisation de la base kaso

---
+++ scripts/init_database.sh
@@ -0,0 +1,7 @@
+source ../../config.sh
+
+mysql -h localhost -u $dbUser -p $dbPassword < articles.sql
+mysql -h localhost -u $dbUser -p $dbPassword < famille.sql
+mysql -h localhost -u $dbUser -p $dbPassword < fournisseur.sql
+mysql -h localhost -u $dbUser -p $dbPassword < stocks.sql
+mysql -h localhost -u $dbUser -p $dbPassword < ventes.sql
