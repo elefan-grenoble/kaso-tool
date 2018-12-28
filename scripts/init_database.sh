ac48877e00ee85995db787c52c4578eb71319aa8
Bastien Deshayes <deshayeb@gmail.com>
2018-12-28 01:26:17 +0100

    Correction du script d'initialisation de la base

--- scripts/init_database.sh
+++ scripts/init_database.sh
@@ -1,7 +1,7 @@
 source ../../kaso-tool-config.sh
 
-mysql -h localhost -u $dbUser -p $dbPassword $dbName < articles.sql
-mysql -h localhost -u $dbUser -p $dbPassword $dbName < famille.sql
-mysql -h localhost -u $dbUser -p $dbPassword $dbName < fournisseur.sql
-mysql -h localhost -u $dbUser -p $dbPassword $dbName < stocks.sql
-mysql -h localhost -u $dbUser -p $dbPassword $dbName < ventes.sql
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < articles.sql
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < famille.sql
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < fournisseur.sql
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < stocks.sql
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < ventes.sql
