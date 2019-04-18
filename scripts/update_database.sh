8ea908d20857fbbda9a81efde3f548941865427a
Bastien Deshayes <bastien.deshayes@kelkoogroup.com>
2019-04-18 18:03:52 +0200

    Script to update database

---
+++ scripts/update_database.sh
@@ -0,0 +1,18 @@
+source ../../kaso-tool-config.sh
+
+echo 'Populating the fournisseur table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/fournisseur.sql
+echo 'Populating the famille table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/famille.sql
+echo 'Populating the rayon table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/rayon.sql
+echo 'Populating the emplacement table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/emplacement.sql
+echo 'Populating the marque table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/marque.sql
+echo 'Populating the provenance table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/provenance.sql
+echo 'Populating the articles table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/articles.sql
+echo 'Populating the stocks table'
+mysql -h localhost -u $dbUser -p$dbPassword $dbName < sql/create/stocks.sql
