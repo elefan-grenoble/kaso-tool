b473d59b5781425369bd1f51d0da8817fa523d43
Yoann Laissus <yoann.laissus@gmail.com>
2019-04-28 17:37:04 +0200

    Fix the STATREGLT table

--- scripts/sql/create/statreglt.sql
+++ scripts/sql/create/statreglt.sql
@@ -1,9 +1,10 @@
 
 CREATE TABLE IF NOT EXISTS STATREGLT (
-  date                   DATE PRIMARY KEY,
+  date                   DATE,
   id_reglement           DECIMAL NOT NULL,
   libelle_reglement      VARCHAR(255) NOT NULL,
-  amount                 INT NOT NULL
+  amount                 INT NOT NULL,
+  PRIMARY KEY (date, id_reglement)
 )
   DEFAULT CHARACTER SET UTF8
   COLLATE UTF8_unicode_ci
